d4cce54b6fd1664ba8bdd4288132f0d5c2beddea
Ignacio <ignacionazar9@gmail.com>
2025-05-20 18:20:55 -0300

    agregue que el reloj tenga la fecha y hora actual y sea dinamico con ceros adelante para que sea mejor visualmente

---
+++ ejercicio4.js
@@ -0,0 +1,49 @@
+const obtenerReloj = () => {
+  const fecha = new Date();
+  const semana = [
+    "Domingo",
+    "Lunes",
+    "Martes",
+    "Miercoles",
+    "Jueves",
+    "Viernes",
+    "Sabado",
+  ];
+  const meses = [
+    "Enero",
+    "Febrero",
+    "Marzo",
+    "Abril",
+    "Mayo",
+    "Junio",
+    "Julio",
+    "Agosto",
+    "Septiembre",
+    "Octubre",
+    "Noviembre",
+    "Diciembre",
+  ];
+  const detalleFecha = document.querySelector("h2");
+  detalleFecha.textContent = `${semana[fecha.getDay()]} ${fecha.getDate()} de ${
+    meses[fecha.getMonth()]
+  } del ${fecha.getFullYear()}`;
+
+let horas = fecha.getHours()
+let minutos = fecha.getMinutes()
+let segundos = fecha.getSeconds()
+
+if(fecha.getHours()<10){
+horas = "0" + horas
+}
+if(fecha.getMinutes()<10){
+minutos = "0" + minutos
+}
+if(fecha.getSeconds()<10){
+segundos = "0" + segundos
+}
+
+  const detalleHora = document.querySelector(".fs-1");
+  detalleHora.textContent = `${horas} : ${minutos} : ${segundos}`;
+};
+
+setInterval(obtenerReloj, 1000);
