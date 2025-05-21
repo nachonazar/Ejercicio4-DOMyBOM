6a214ee0e0cd7573c92a32b6b5c1c1d52939e943
Ignacio <ignacionazar9@gmail.com>
2025-05-21 00:52:48 -0300

    agregue mejoras visuales al reloj

--- ejercicio4.js
+++ ejercicio4.js
@@ -28,22 +28,27 @@ const obtenerReloj = () => {
     meses[fecha.getMonth()]
   } del ${fecha.getFullYear()}`;
 
-let horas = fecha.getHours()
-let minutos = fecha.getMinutes()
-let segundos = fecha.getSeconds()
+  let horas = fecha.getHours();
+  let minutos = fecha.getMinutes();
+  let segundos = fecha.getSeconds();
 
-if(fecha.getHours()<10){
-horas = "0" + horas
-}
-if(fecha.getMinutes()<10){
-minutos = "0" + minutos
-}
-if(fecha.getSeconds()<10){
-segundos = "0" + segundos
-}
+  let horario = "AM";
+  if (horas >= 12) {
+    horario = "PM";
+  }
+
+  if (fecha.getHours() < 10) {
+    horas = "0" + horas;
+  }
+  if (fecha.getMinutes() < 10) {
+    minutos = "0" + minutos;
+  }
+  if (fecha.getSeconds() < 10) {
+    segundos = "0" + segundos;
+  }
 
   const detalleHora = document.querySelector(".fs-1");
-  detalleHora.textContent = `${horas} : ${minutos} : ${segundos}`;
+  detalleHora.textContent = `${horas} : ${minutos} : ${segundos} (${horario})`;
 };
 
 setInterval(obtenerReloj, 1000);
